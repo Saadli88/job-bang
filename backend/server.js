ee18d2fde438ec30e49b827fec8ed20eaf36e6b0
rothmony <rothmony.chhem@gmail.com>
2024-09-24 14:41:55 -0400

    ajoute data ds mango via data front

--- backend/server.js
+++ backend/server.js
@@ -24,3 +24,25 @@ app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 //Commande a faire dans le dossier backend
 //npm install express mongoose bcryptjs jsonwebtoken
+
+// ajoute data
+app.post('/api/data', async (req, res) => {
+    try {
+      const existingEntries = await DataModel.find({ 
+        name: { $in: req.body.map(entry => entry.name) }
+      });
+  
+      const existingNames = existingEntries.map(entry => entry.name);
+      const newDataEntries = req.body.filter(entry => !existingNames.includes(entry.name));
+  
+      if (newDataEntries.length > 0) {
+        const savedEntries = await DataModel.insertMany(newDataEntries);
+        return res.status(201).send(savedEntries);
+      } else {
+        return res.status(200).send({ message: 'No new data to add' });
+      }
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  });
+  
